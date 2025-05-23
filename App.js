a5d9f92ecb8c90db11166c464ec6d0916308f1cf
MATIAS DANIEL HALAC <48792865@ort>
2025-05-23 12:16:41 -0300

    falta poco

---
+++ App.js
@@ -0,0 +1,55 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+
+const imgMedio = require('./assets/FotoMedio.png')
+
+export default function App() {
+  return (
+    <>
+      <View style={styles.header}>
+        <Text style={{textAlign: 'center', marginTop: '20%', color: 'white', fontSize: 20}} >Login App (Halac)</Text>
+      </View>
+      <Image source = {imgMedio} style={styles.image}></Image>
+      <TextInput style={styles.inputMail} placeholder='Email' />
+      <TextInput secureTextEntry style={styles.inputPass} placeholder='Contraseña'/>
+      <TouchableOpacity style={{backgroundColor: '#9455f4', marginTop: '8%', width: '80%'}}><Text style={{textAlign: 'center', color: 'white'}}>INGRESAR</Text></TouchableOpacity>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#9455f4',
+    height: '13%'
+  },
+  image: {
+    alignSelf: 'center',
+    marginTop: '10%',
+  },
+  inputMail: {
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    width: '80%',
+    height: '5%',
+    alignSelf: 'center',
+    marginTop: '8%',
+    padding: 10,
+  },
+  inputPass: {
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    width: '80%',
+    height: '5%',
+    alignSelf: 'center',
+    marginTop: '2%',
+    padding: 10,
+  },
+});
